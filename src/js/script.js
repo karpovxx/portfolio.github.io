82efaec507d8973315e5fd6512cee5175c625d77
Ruslan Karpov <ruslankarpov64@gmail.com>
2024-05-11 19:07:21 +0500

    eighth

--- src/js/script.js
+++ src/js/script.js
@@ -17,19 +17,4 @@ console.log(counters);
 counters.forEach((item, i) => {
     lines [i].style.width = item.innerHTML;
 
-$(window).scroll(function() {
-		if ($(this).scrollTop() > 1600) {
-			$('.pageup').fadeIn();			
-		}
-		else {
-			$('.pageup').fadeOut();
-		}
-	});
-	$("a[href^='#up']").click(function(){
-		var _href = $(this).attr("href");
-		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-		return false;
-	});
-
-	new WOW().init()
 });
